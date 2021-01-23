2475cdef10a8eb890515e521b097924619ac7073
Matthew Milici <milici.m915@gmail.com>
2021-01-22 21:51:51 -0800

    working

---
+++ client/build/precache-manifest.29079d437bc4c228ad2c5e4fe22aadde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "526b788db7d9f27c0633",
+    "url": "/static/css/main.046cee60.chunk.css"
+  },
+  {
+    "revision": "526b788db7d9f27c0633",
+    "url": "/static/js/main.b86e0429.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0bedf1b42555cb6b75ef",
+    "url": "/static/js/2.7885ef80.chunk.js"
+  },
+  {
+    "revision": "d32d286ed402ce45a70454e4be7cb7f4",
+    "url": "/index.html"
+  }
+];
